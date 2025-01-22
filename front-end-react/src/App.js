23f71e14ca0c9cdad7b9e5cb203e34dcab941fe0
heavenlycreature <lepiinnn@gmail.com>
2025-01-22 15:31:20 +0000

    title change (again)

--- front-end-react/src/App.js
+++ front-end-react/src/App.js
@@ -18,7 +18,7 @@ function GetStarted(props) {
 
   return (<Card>
     <Card.Body>
-      <Card.Title>Let's Having Fun!</Card.Title>
+      <Card.Title>Let's Having Fun Together!</Card.Title>
       <Card.Text>
         Click the button below to start a new game.
       </Card.Text>
